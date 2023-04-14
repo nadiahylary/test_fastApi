5da94a6c9c9524679259c0616bcfe476f57dfc4a
Nadia HYLARY <nhylary12@gmail.com>
2023-04-14 15:58:44 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/age")
+async def your_age(age: int):
+    return {"message": f"You are {age} years old."}
